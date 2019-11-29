feb28d0321039ba7b2a953faa3d2bdb20421f3f0
Nelson Nyland <nelsonnyland@gmail.com>
2019-11-28 16:59:18 -0800

    Changes to scripts.

--- scripts/news.js
+++ scripts/news.js
@@ -1,33 +1,3 @@
-// console.log("NEWS CONNECTED");
-"use strict";
-/**
- * news.js
- * This API connects to my GitHub profile and gets
- * the current newsfeed.
- * @author Nelson Nyland
- * @version 1.0 (Nov 20, 2019)
- */
-
-const issuesURL = "https://api.github.com/search/issues?q=author:nelsonnyland:";
-const commitsURL = "https://api.github.com/search/commits?q=author:nelsonnyland:";
-const feedsURL = "https://api.github.com/feeds";
-const profileURL = "https://api.github.com/users/nelsonnyland";
-const activitiesURL = "https://api.github.com/users/nelsonnyland/events/public";
-
-// fetch api data
-async function getData(url) {
-    let response = await fetch(url, {
-        "method" : "GET",
-        "headers" : {
-            "Authorization" : `${process.env.TOKEN}`
-        }
-    });
-    let data = await response.json();
-    // console logs tests:
-    console.log("getData:");
-    console.log(data);
-    return data;
-}
 
 // display data to newsfeed
 function postNews(data) {
@@ -39,9 +9,3 @@ function postNews(data) {
     //article.appendChild(document.createTextNode(JSON.stringify(data)));
 }
 
-window.onload = function() {
-    if (PRODUCTION == "false") {
-        getToken();
-    }
-    getData(activitiesURL)//.then(data => postNews(data));
-}
