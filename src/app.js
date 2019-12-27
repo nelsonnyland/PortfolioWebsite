f5cfe6b4a44bf4a3d20a97b967c441bf938abf79
Nelson Nyland <nelsonnyland@gmail.com>
2019-12-26 18:01:32 -0800

    Api update.

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ require("dotenv").config();
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const request = require("request");
 
-const api = require("./api.js");
+//const api = require("./api.js");
 
 const app = express();
 
@@ -20,15 +21,51 @@ hbs.registerPartials(partDir);
 
 app.use(express.static(pubDir));
 
+const authorization = {
+    "User-Agent": "request",
+    "Authorization": `TOKEN ${process.env.TOKEN}`
+};
+
 app.get("", (req, res) => {
-    //const repos = api.getRepos();
-    res.render("index");
+    const options = {
+        url: "https://api.github.com/users/nelsonnyland/repos",
+        headers: authorization
+    };
+    
+    request(options, callback);
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body);
+            //console.log("DATA: " + JSON.stringify(data));
+            res.send(data);
+        } else {
+            console.log("ERROR" + error);
+            console.log("RESPONSE" + response);
+            console.log("BODY" + body);
+        }
+    }
 });
 
 app.get("/news", (req, res) => {
-    //const news = api.getNews();
-    //res.render("news");
+    const options = {
+        url: "https://api.github.com/users/nelsonnyland/events/public",
+        headers: authorization
+    };
+    
+    request(options, callback);
 
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body);
+            //console.log("DATA: " + JSON.stringify(data));
+            res.send(data);
+        } else {
+            console.log("ERROR" + error);
+            console.log("RESPONSE" + response);
+            console.log("BODY" + body);
+        }
+    }
 });
 
 app.get("/contact", (req, res) => {
