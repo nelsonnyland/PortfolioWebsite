08452b2a59934bc9e56ac6280aaa49fbf9156aff
Nelson Nyland <nelsonnyland@gmail.com>
2020-01-27 20:47:58 -0800

    Client-side js for news page.

--- public/scripts/news.js
+++ public/scripts/news.js
@@ -2,18 +2,41 @@
 // client-side js
 
 const url = "https://api.github.com/users/nelsonnyland/events/public";
+const status = document.getElementById("status");
 const news = document.getElementById("news");
 
-news.textContent = "Loading...";
-
-// http://localhost:3000/api?address=https://api.github.com/users/nelsonnyland/events/public
 fetch("http://localhost:3000/api?address=" + url).then((response) => {
     response.json().then((data) => {
-        data = JSON.stringify(data);
+        while (news.firstChild) {
+            news.removeChild(news.firstChild);
+        }
+        status.textContent = "Loading..."
         if (data.error) {
-            news.textContent = data.error;
+            status.textContent = "Error loading page."
+            console.log(data.error);
         } else {
-            news.textContent = data;
+            for (let i = 0; i < data.length; i++) {
+                // create type node
+                let liHeader = document.createElement("li");
+                let typeStr = JSON.stringify(data[i].type);
+                let removeEvent = typeStr.replace("Event", "");
+                liHeader.appendChild(document.createTextNode(replace(removeEvent)));
+                // create sub list
+                let ulSub = document.createElement("ul");
+                liHeader.appendChild(ulSub);
+                // create repo node
+                let liRepo = document.createElement("li");
+                let repoStr = "repo: " + JSON.stringify(data[i].repo.name);
+                liRepo.appendChild(document.createTextNode(replace(repoStr)));
+                ulSub.appendChild(liRepo);
+                // append nodes to news
+                news.appendChild(liHeader);
+            }
+            status.textContent = "";
         }
     });
 });
+
+function replace(str) {
+    return str.replace(/\"/g, "");
+}
